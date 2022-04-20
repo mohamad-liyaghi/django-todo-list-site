b67c925e7b1b19e16de9b682a32eb3d9672b4680
Ml06py <liaghimohamad69@gmail.com>
2022-04-20 22:27:49 +0430

    humanize date added

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'rest_framework',
     'apps.account',
     'apps.todo',
