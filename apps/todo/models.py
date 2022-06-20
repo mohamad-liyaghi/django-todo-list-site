ffe1f8b452b2236b39e04dd582d19416eb9fcab3
Ml06py <liaghimohamad69@gmail.com>
2022-06-20 17:50:04 +0430

    routine model added

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -25,3 +25,21 @@ class project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class week(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class routine(models.Model):
+    title = models.CharField(max_length=50)
+    detail = models.TextField(blank=True)
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    token = models.CharField(max_length=12, unique=True, blank=True)
+    time = models.TimeField()
+    days = models.ManyToManyField(week, blank=True)
+
+    def __str__(self):
+        return self.title
