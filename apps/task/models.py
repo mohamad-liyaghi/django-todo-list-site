bdfb3a8af09daa73da3a95057d2cba187be128cb
Ml06py <liaghimohamad69@gmail.com>
2022-09-10 23:36:44 +0430

    Base Model added

--- apps/task/models.py
+++ apps/task/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from accounts.models import User
-from datetime import date
+
+
+class Base(models.Model):
+    '''Common fields of Task, Routine and projects model'''
+
+    class Status(models.TextChoices):
+        u = ("u", "Unfinished ")
+        f = ("f", "Finished")
+        d = ("d", "Doing")
+
+    title = models.CharField(max_length=120)
+    detail = models.TextField()
+
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    token = models.CharField(max_length=12, unique=True, blank=True, null=True)
+
+    time = models.DateTimeField()
+
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
 
 # Create your models here.
 class task(models.Model):
