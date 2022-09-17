cadd9f2612e436414d8bcc18ba81da69e9420fbe
Ml06py <liaghimohamad69@gmail.com>
2022-09-17 13:03:18 +0430

    Token Life time configed

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import sys
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -189,6 +190,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME" : timedelta(days=1),
 }
 
 DJOSER = {
